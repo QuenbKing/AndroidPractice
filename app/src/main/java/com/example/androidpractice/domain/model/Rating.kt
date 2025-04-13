f5d881d2a16dd0d0297a20de8f037a089314feff
QuenbKing <Nikita.Karskanov@urfu.me>
2025-04-14 00:42:43 +0500

    [Working with the network] - KarskanovNA - done (Добавил @SerializedName("kp"), чтобы тянулись данные по оценке от кинопоиска из API)

--- app/src/main/java/com/example/androidpractice/domain/model/Rating.kt
+++ app/src/main/java/com/example/androidpractice/domain/model/Rating.kt
@@ -1,6 +1,9 @@
 package com.example.androidpractice.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Rating(
+    @SerializedName("kp")
     val kinopoisk: Double,
     val imdb: Double,
     val rottenTomatoes: Double,
